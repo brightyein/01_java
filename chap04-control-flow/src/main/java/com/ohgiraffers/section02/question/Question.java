33ed70949a34279c7954ebbb618bed206dd985b6
KimYeIn <wmfrjqsp@gmail.com>
2024-05-13 17:30:26 +0900

    [240513] literal and variable

---
+++ chap04-control-flow/src/main/java/com/ohgiraffers/section02/question/Question.java
@@ -0,0 +1,20 @@
+package main.java.com.ohgiraffers.section02.question;
+
+import java.util.Scanner;
+
+public class Question {
+    public static void main(String[] args) {
+        /*
+        * 입력한 숫자 범위 내에서 소수를 찾아 출력하는 프로그램을 작성 해봅시다.
+        * 사용자로부터 시작 숫자와 끝 숫자를 입력 받습니다. 프로그램은 해당 범위 내에 존재하는 모든 소수를 출력합니다.
+        * 소수 =1 과 자기 자신을 제외한 수로 나누어 떨어지지 않는 수
+        * */
+
+        Scanner sc = new Scanner(System.in);
+        int startNum = sc.nextInt();
+        int endNum = sc.nextInt();
+
+        boolean isPrime = true;
+
+    }
+}
