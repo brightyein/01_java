688eb11386db75f97957f6b26b2cf4f9da45e2ec
KimYeIn <wmfrjqsp@gmail.com>
2024-05-23 17:21:08 +0900

    [chap12] 백준 9375번 패션왕 신해빈 푸는중

--- chap12-collection-lecture/src/main/java/com/ohgiraffers/section03/map/HashMapQuestion.java
+++ chap12-collection-lecture/src/main/java/com/ohgiraffers/section03/map/HashMapQuestion.java
@@ -9,51 +9,61 @@ import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class HashMapQuestion {
+
+    // 몇 번 구했는지
+    static int count = 0;
+
+    static Scanner sc = new Scanner(System.in);
+
+    // 몇 가지 케이스를 계산할건지
+    static int totalCase = sc.nextInt();
+
+
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
 
-        // 몇 번 구했는지
-        int count = 0;
+    }
 
-        // 몇 가지 케이스를 계산할건지
-        int totalCase = sc.nextInt();
+    // 입력받는 메서드
+    public static Map<String, String> inputInfo() {
 
-        while (count != totalCase) {
+        // case1 의 의상 갯수
+        int case1Clothes = sc.nextInt();
+        sc.nextLine();
 
-            // case1 의 의상 갯수
-            int case1Clothes = sc.nextInt();
-            sc.nextLine();
+        Map<String, String> clothesMap1 = new HashMap<String, String>();
 
-            Map<String, String> clothesMap1 = new HashMap<String, String>();
+        for (int i = 0; i < case1Clothes; i++) {
 
-            for (int i = 0; i < case1Clothes; i++) {
+            // 공백을 기준으로 잘라서 Map 에 담는다
+            String[] split = sc.nextLine().split(" ");
 
-                // 공백을 기준으로 잘라서 Map 에 담는다
-                String[] split = sc.nextLine().split(" ");
-
-                // key : 의상이름 , value : 의상종류
-                clothesMap1.put(split[0], split[1]);
-            }
-
-            // key : value , value : 갯수
-            Map<String, Long> valueCountMap = clothesMap1.values().stream()
-                    .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
-
-            if (valueCountMap.size() == 1) {
-                System.out.println(case1Clothes);
-                count++;
-
-            } else {
-
-                long result = 1;
-                for (Map.Entry<String, Long> entry : valueCountMap.entrySet()) {
-
-                    result *= (entry.getValue()); // 경우의 수 곱
-                } System.out.println(result + case1Clothes);
-
-                count++; // case 처리 횟수 카운트
-            }
+            // key : 의상이름 , value : 의상종류
+            clothesMap1.put(split[0], split[1]);
         }
+        return clothesMap1;
+    }
+
+    // 경우의 수 세는 메서드
+    public static void checkCase(Map<String, String> clothesMap1) {
+        // key : value , value : 갯수 (같은 의상 종류가 몇개인지 세어 맵으로 저장)
+        Map<String, Long> valueCountMap = clothesMap1.values().stream()
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+
+        if (valueCountMap.size() == 1) {
+            System.out.println(case1Clothes);
+            count++;
+
+        } else {
+
+            long result = 1;
+            for (Map.Entry<String, Long> entry : valueCountMap.entrySet()) {
+
+                result *= (entry.getValue()); // 경우의 수 곱
+            } System.out.println(result + case1Clothes);
+
+            count++; // case 처리 횟수 카운트
+        }
+
     }
 }
